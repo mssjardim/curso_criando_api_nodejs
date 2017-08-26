f190abd9e6a9efd8a3eea5e33468f65ec7d740ac
Marco Jardim <santarelle@gmail.com>
2017-08-25 22:49:18 -0400

    List products, get product by slugs

--- node-str/src/controllers/product-controller.js
+++ node-str/src/controllers/product-controller.js
@@ -2,18 +2,43 @@
 const mongoose = require('mongoose')
 const Product = mongoose.model('Product')
 
+exports.get = (req, res, next) => {
+    Product
+        .find({
+            active: true
+        }, 'title price slug')
+        .then(data => {
+            res.status(200).send(data)
+        }).catch(e => {
+            res.status(400).send(e)
+        })
+}
+
+exports.getBySlug = (req, res, next) => {
+    Product
+        .findOne({
+            slug: req.params.slug,
+            active: true
+        }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data)
+        }).catch(e => {
+            res.status(400).send(e)
+        })
+}
+
 exports.post = (req, res, next) => {
     let product = new Product(req.body)
     product.save()
         .then(x => {
             res.status(201).send({
                 message: 'Product created successfully'
-            });
+            })
         }).catch(e => {
             res.status(400).send({
                 message: 'Failed to create product',
                 data: e
-            });
+            })
         });
 }
 
