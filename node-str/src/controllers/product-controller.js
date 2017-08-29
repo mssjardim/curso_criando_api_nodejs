3c436f7c3f4f7a13b668b08fc72633c49dd0a34a
Marco Jardim <msantarelle@instituto-jc.org.br>
2017-08-29 16:38:09 -0400

    adding repositories to separated controllers to data access (daos, repository)

--- node-str/src/controllers/product-controller.js
+++ node-str/src/controllers/product-controller.js
@@ -1,14 +1,11 @@
 'use strict'
-const mongoose = require('mongoose')
-const Product = mongoose.model('Product')
 
 const ValidationContract = require('../validators/fluent-validator')
+const repository = require('../repositories/product-repository')
 
 exports.get = (req, res, next) => {
-    Product
-        .find({
-            active: true
-        }, 'title price slug')
+    repository
+        .get()    
         .then(data => {
             res.status(200).send(data)
         }).catch(e => {
@@ -17,11 +14,8 @@ exports.get = (req, res, next) => {
 }
 
 exports.getBySlug = (req, res, next) => {
-    Product
-        .findOne({
-            slug: req.params.slug,
-            active: true
-        }, 'title description price slug tags')
+    repository
+        .getBySlug(req.params.slug)
         .then(data => {
             res.status(200).send(data)
         }).catch(e => {
@@ -30,8 +24,8 @@ exports.getBySlug = (req, res, next) => {
 }
 
 exports.getById = (req, res, next) => {
-    Product
-        .findById(req.params.id)
+   repository
+        .getById(req.params.id)
         .then(data => {
             res.status(200).send(data)
         }).catch(e => {
@@ -40,11 +34,8 @@ exports.getById = (req, res, next) => {
 }
 
 exports.getByTag = (req, res, next) => {
-    Product
-        .find({
-            tags: req.params.tag,
-            active: true
-        }, 'title description price slug tags')
+    repository
+        .getByTag(req.params.tag)
         .then(data => {
             res.status(200).send(data)
         }).catch(e => {
@@ -62,8 +53,8 @@ exports.post = (req, res, next) => {
         return;
     }
 
-    let product = new Product(req.body)
-    product.save()
+    repository
+        .post(req.body)
         .then(x => {
             res.status(201).send({
                 message: 'Product created successfully'
@@ -77,15 +68,9 @@ exports.post = (req, res, next) => {
 }
 
 exports.put = (req, res, next) => {
-    Product
-        .findByIdAndUpdate(req.params.id, {
-            $set: {
-                title: req.body.title,
-                description: req.body.description,
-                price: req.body.price,
-                slug: req.body.slug
-            }
-        }).then(x => {
+    repository
+        .put(req.params.id, req.body)
+        .then(x => {
             res.status(200).send({
                 message: 'Product updated sucessfully'
             })
@@ -98,8 +83,8 @@ exports.put = (req, res, next) => {
 }
 
 exports.delete = (req, res, next) => {
-    Product
-        .findByIdAndRemove(req.body.id)
+    repository
+        .delete(req.body.id)
         .then(x => {
             res.status(200).send({
                 message: 'Product removed sucessfully'
